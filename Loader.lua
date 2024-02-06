c794e53b81eb533c26413ea9cc46f742d7c4daf1
Hacker3331 <139943514+Hacker3331@users.noreply.github.com>
2024-02-06 17:59:45 +0700

    Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -2,7 +2,7 @@ local GameId = game.GameId
 function loadurl(url)
     loadstring(game:HttpGet(url))()
 end
-repo = 'https://raw.githubusercontent.com/Hacker3331/HackerHub-Games/main/Games/' --Link Of Folder Contains Script
+repo = 'https://raw.githubusercontent.com/Hacker3331/HackerHub-Games/main/Games/'
 
 loadurl(repo.. 'HackerAdmin.lua')
 
