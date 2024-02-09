0206246378f20b4b87c46c0386191ddb157aabf0
Hacker3331 <139943514+Hacker3331@users.noreply.github.com>
2024-02-09 07:50:13 +0700

    HackerAdminGames.lua

---
+++ HackerAdminGames.lua
@@ -0,0 +1,7 @@
+local GameId = game.GameId
+function loadurl(url)
+    loadstring(game:HttpGet(url))()
+end
+repo = 'https://raw.githubusercontent.com/Hacker3331/HackerHub-Games/main/Games/'
+loadurl(repo..'HackerAdmin.lua')
+end
