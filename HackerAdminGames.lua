1a6efa4680e55a66544741136707bbc529afb150
Hacker3331 <139943514+Hacker3331@users.noreply.github.com>
2024-02-09 13:19:15 +0700

    Delete HackerAdminGames.lua

--- HackerAdminGames.lua
+++
@@ -1,9 +0,0 @@
-local GameId = game.GameId
-function loadurl(url)
-    loadstring(game:HttpGet(url))()
-end
-repo = 'https://raw.githubusercontent.com/Hacker3331/HackerHub-Games/main/Games/'
-
-loadurl(repo..'HackerAdmin.lua')
-
-end
