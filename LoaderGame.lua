5467c7fb49078013926149d57f7685655eccf9e0
Hacker3331 <139943514+Hacker3331@users.noreply.github.com>
2024-02-13 16:38:26 +0700

    LoaderGame.lua

--- LoaderGame.lua
+++ LoaderGame.lua
@@ -1,3 +1,11 @@
+--[[
+    DO NOT COPY THIS CODE AND RUN IT!!!
+    IT WILL NOT WORK IF I ADD NEW GAMES!
+
+    Instead, please go to copy the loadstring version in here or copy it on https://pastebin.com/K0fENkUu and use the code there.
+
+    Thanks.
+]]
 local games = {
     [6516141723] = "Doors",
     [6839171747] = "Doors [In Game]",
