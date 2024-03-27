13fc7066b88f63658cef069b626d5297fba6d109
Hacker3331 <139943514+Hacker3331@users.noreply.github.com>
2024-03-27 16:58:38 +0700

    LoaderGame.lua
    "Murder VS Sherif Duels" has get added to Loader!

--- LoaderGame.lua
+++ LoaderGame.lua
@@ -13,20 +13,27 @@ game:GetService("StarterGui"):SetCore("SendNotification",{
 })
 wait(0.7)
 
-if game.PlaceId == 6516141723 then  --- Doors Lobby
+if game.PlaceId == 6516141723 then  -- Doors Lobby
 game:GetService("StarterGui"):SetCore("SendNotification",{
 	Title = "Hacker Notification", -- Required
 	Text = "Game Support " .. "[" .. GameP .. "]", -- Required
 	Icon = "rbxassetid://1234567890" -- Optional
 })
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Hacker3331/HackerHub-Games/main/Games/Doors.lua"))()
-  elseif game.PlaceId == 6839171747 then --- Doors In Game
+  elseif game.PlaceId == 6839171747 then -- Doors In Game
 game:GetService("StarterGui"):SetCore("SendNotification",{
 	Title = "Hacker Notification", -- Required
 	Text = "Game Support " .. "[" .. GameP .. "]", -- Required
 	Icon = "rbxassetid://1234567890" -- Optional
 })
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Hacker3331/HackerHub-Games/main/Games/Doors%20%5BIn%20Game%5D.lua"))()
+  elseif game.PlaceId == 12355337193 then -- Murder VS Sherif Duels
+game:GetService("StarterGui"):SetCore("SendNotification",{
+	Title = "Hacker Notification", -- Required
+	Text = "Game Support " .. "[" .. GameP .. "]", -- Required
+	Icon = "rbxassetid://1234567890" -- Optional
+})
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Hacker3331/HackerHub-Games/main/Games/Murder%20VS%20Sherif.lua"))()
   else -- Not Support
 game:GetService("StarterGui"):SetCore("SendNotification",{
 	Title = "Hacker Notification", -- Required
