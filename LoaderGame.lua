6cf0279b12dc0e3450449adf9d12472bb75b1a5b
Hacker3331 <139943514+Hacker3331@users.noreply.github.com>
2024-10-12 14:57:43 +0700

    Update LoaderGame.lua

--- LoaderGame.lua
+++ LoaderGame.lua
@@ -33,6 +33,6 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/Hacker3331/HackerHub-
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Hacker3331/HackerHub-Games/refs/heads/main/Games/Super%20Fun%20Obby.lua"))()
   elseif game.PlaceId == 13127800756 then -- Arm Wrestle Simulator
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Hacker3331/HackerHub-Games/refs/heads/main/Games/Arm%20Wrestle%20Simulator.lua"))()
--- Rainbow Friend [All Chapter]
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Hacker3331/HackerHub-Games/refs/heads/main/Games/Rainbow%20Friend%20%5BChapter%202%5D.lua"))()
+else --Not support
+print("Not Support")
 end
